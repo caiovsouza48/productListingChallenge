d0a281a6a8b77fbebe69a62f1a428bd47f9e8978
Caio Vasconcelos <caio.vasconcelos@beblue.com.br>
2020-01-12 12:45:52 -0300

    Implemented Core Framework for Coordinator

---
+++ ProductListMVC/Core/Core/Core.h
@@ -0,0 +1,19 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by Daniel Griso Filho on 10/06/19.
+//  Copyright © 2019 BeBlue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Core.
+FOUNDATION_EXPORT double CoreVersionNumber;
+
+//! Project version string for Core.
+FOUNDATION_EXPORT const unsigned char CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
+
+
