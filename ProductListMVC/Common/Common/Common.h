77f7365ee0fce0085abaac1f36a1eea9eb31d5c1
Caio Vasconcelos <caio.vasconcelos@beblue.com.br>
2020-01-14 10:55:38 -0300

    Commit Common.framework

---
+++ ProductListMVC/Common/Common/Common.h
@@ -0,0 +1,19 @@
+//
+//  Common.h
+//  Common
+//
+//  Created by Caio de Souza on 12/01/20.
+//  Copyright © 2020 Caio de Souza. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Common.
+FOUNDATION_EXPORT double CommonVersionNumber;
+
+//! Project version string for Common.
+FOUNDATION_EXPORT const unsigned char CommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Common/PublicHeader.h>
+
+
